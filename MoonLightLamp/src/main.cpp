d898ff78cfe83b4a1f8d17a4d5bad12faf5086cf
Jaclyn Chen <jaclyn.chen5@gmail.com>
2022-05-12 01:25:01 -0700

    added button to turn on and off with onePixel animation

--- MoonLightLamp/src/main.cpp
+++ MoonLightLamp/src/main.cpp
@@ -1,13 +1,22 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <ezButton.h>
 
 #define PIN 7
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(144, PIN, NEO_RGBW + NEO_KHZ800);
 
+ezButton limitSwitch(2);
+
+boolean lightState = false; 
+
 //define functions
 void onePixel(uint8_t wait);
+void fillWhite(); 
+void allOff(); 
+
 
 void setup() {
+  limitSwitch.setDebounceTime(50); // set debounce time to 50 milliseconds
   strip.begin();
   pinMode(PIN,OUTPUT);
   strip.show(); // Initialize all pixels to 'off'
@@ -20,8 +29,37 @@ void loop() {
   //colorSet(strip.Color(255, 255, 255, 255), 100);  //Green
   //onePixel(10); 
 
-  onePixel(10);
+
+  limitSwitch.loop(); // MUST call the loop() function first
+ if(limitSwitch.isPressed()){
+    Serial.println("The limit switch: UNTOUCHED -> TOUCHED");
+    lightState = !lightState; 
+    if(lightState){
+      onePixel(10);
+    }
+    else{
+      allOff();
+    }
+    
+ }
+
+ 
+  if(limitSwitch.isReleased()){
+    Serial.println("The limit switch: TOUCHED -> UNTOUCHED");
+
+  }
+  /*
+  int state = limitSwitch.getState();
+  if(state == HIGH)
+	  Serial.println("The limit switch: UNTOUCHED");
+  else
+	  Serial.println("The limit switch: TOUCHED");*/
+
+
+  //onePixel(10);
+  //fillWhite();
   digitalWrite(PIN, HIGH);
+ 
 }   
 
 
@@ -45,3 +83,19 @@ void onePixel(uint8_t wait) {
     }
      
 }
+void fillWhite(){
+   for(int i = 0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, 0, 255, 255, 255);
+   }
+    strip.show();
+  
+}
+
+void allOff(){
+   
+    strip.clear();
+    strip.show();
+  
+}
+
+
